5f8ab1498bf805197122ec86a81685fad43a08ea
Niteshraiss <niteshrai182@gmail.com>
2025-04-08 09:49:43 +0530

    lets go

---
+++ steps/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [step, setStep] = useState(1)
+
+  function handlePrevious() {
+    if(step > 1) setStep(prev => prev - 1);
+    console.log(step)
+  }
+  function handleNext() {
+    if (step < 3) setStep(prev => prev + 1);
+    console.log(step)
+  }
+  const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+  ];
+  return (
+    <>
+      <div className='steps'>
+        <div className="numbers">
+          <div className={step >= 1 ? 'active' : ''}>1</div>
+          <div className={step >= 2 ? 'active' : ''}>2</div>
+          <div className={step >= 3 ? 'active' : ''}>3</div>
+        </div>
+        <p className="message">
+          Step {step}: {messages[step -1]}
+        </p>
+        <div className="buttons">
+          <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={() => { handlePrevious() }}>
+            Previous
+          </button>
+          <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={() => { handleNext() }}>
+            Next
+          </button>
+        </div>
+      </div >
+    </>
+  )
+}
+
+export default App
